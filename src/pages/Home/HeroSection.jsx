27258540f840d4b9deae039d82ef013bc1b315af
Roy <roypaul.narido@g.msuiit.edu.ph>
2024-02-18 14:24:00 +0800

    add: home business div section

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -11,49 +11,49 @@ export default function HeroSection() {
                         </div>
                     </div>
                     <div className="hero-section-btn">
-                        <button data-aos="fade-right" data-aos-delay="900">About Us</button>
-                        <button data-aos="fade-left" data-aos-delay="900">Get in Touch</button>
+                        <button data-aos="fade-right" data-aos-delay="400">About Us</button>
+                        <button data-aos="fade-left" data-aos-delay="400">Get in Touch</button>
                     </div>
                 </div>
                 <div className="hero-section-right-container">
                     <div className="hero-section-vid-column">
-                    <div className="hero-section-vid-container container-vid-big" data-aos="fade-down" data-aos-delay="1100">
+                    <div className="hero-section-vid-container container-vid-big" data-aos="fade-down" data-aos-delay="700">
                             <video src="./videos/vertex-vid1.mp4" autoPlay loop muted />
                     </div>
-                    <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="1250">
+                    <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="850">
                             <video src="./videos/vertex-vid2.mp4" autoPlay loop muted />
                     </div>
                     </div>
                     
                     <div className="hero-section-vid-column">
-                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="1700">
+                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="900">
                             <video src="./videos/vertex-vid3.mp4" autoPlay loop muted />
                         </div>
-                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="1550">
+                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="1050">
                                 <video src="./videos/vertex-vid4.mp4" autoPlay loop muted />
                         </div>
-                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="1400">
+                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="1200">
                                 <video src="./videos/vertex-vid5.mp4" autoPlay loop muted />
                         </div>
                     </div>
 
                     <div className="hero-section-vid-column">
-                        <div className="hero-section-vid-container container-vid-small"  data-aos="fade-down" data-aos-delay="1950">
+                        <div className="hero-section-vid-container container-vid-small"  data-aos="fade-down" data-aos-delay="1400">
                                 <video src="./videos/vertex-vid6.mp4" autoPlay loop muted />
                         </div>
-                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="2100">
+                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="1550">
                                 <video src="./videos/vertex-vid7.mp4" autoPlay loop muted />
                         </div>
-                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="2250">
+                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="1700">
                                 <video src="./videos/vertex-vid8.mp4" autoPlay loop muted />
                         </div>
                     </div>
                     
                     <div className="hero-section-vid-column">
-                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="2600">
+                        <div className="hero-section-vid-container container-vid-small" data-aos="fade-down" data-aos-delay="2050">
                             <video src="./videos/vertex-vid9.mp4" autoPlay loop muted />
                     </div>
-                    <div className="hero-section-vid-container container-vid-big" data-aos="fade-down" data-aos-delay="2450">
+                    <div className="hero-section-vid-container container-vid-big" data-aos="fade-down" data-aos-delay="1900">
                             <video src="./videos/vertex-vid11.mp4" autoPlay loop muted />
                     </div>
                     </div>
